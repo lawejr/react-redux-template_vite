1a3469d9f305a8d2c763c49a4a73b9d92de11724
Aleksandr Kalinin <dev.lawejr@gmail.com>
2024-03-12 18:45:16 +0500

    Favicon and manifest auto-generator was added

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import reactPlugin from '@vitejs/plugin-react';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import faviconsInjectPlugin from 'vite-plugin-favicons-inject';
 import imageminPlugin from 'vite-plugin-imagemin';
+// @ts-ignore
+import { version } from './package.json';
 
 const imageminConfig: unknown = {
   gifsicle: false,
@@ -24,6 +27,49 @@ const imageminConfig: unknown = {
       },
     ],
   },
+};
+
+// config from internal used package https://www.npmjs.com/package/favicons
+const faviconsConfig = {
+  appName: 'Example APP',
+  appShortName: 'Example APP',
+  appDescription: '',
+  lang: 'ru-RU', // Primary language for name and short_name
+  background: '#fbfafc', // Background colour for flattened icons. `string`
+  theme_color: '#fbfafc', // Theme color user for example in Android's task switcher. `string`
+  appleStatusBarStyle: 'default', // Style for Apple status bar: "black-translucent", "default", "black". `string`
+  display: 'standalone', // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
+  start_url: '.', // Start URL when launching the application from a device. `string`
+  preferRelatedApplications: false, // Should the browser prompt the user to install the native companion app. `boolean`
+  relatedApplications: undefined, // Information about the native companion apps. This will only be used if `preferRelatedApplications` is `true`. `Array<{ id: string, url: string, platform: string }>`
+  version: version, // Your application's version string. `string`
+  icons: {
+    // Platform Options:
+    // - offset - offset in percentage
+    // - background:
+    //   * false - use default
+    //   * true - force use default, e.g. set background for Android icons
+    //   * color - set background for the specified icons
+    //
+    android: true, // Create Android homescreen icon. `boolean` or `{ offset, background }` or an array of sources
+    appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background }` or an array of sources
+    appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background }` or an array of sources
+    favicons: true, // Create regular favicons. `boolean` or `{ offset, background }` or an array of sources
+    windows: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background }` or an array of sources
+    yandex: true, // Create Yandex browser icon. `boolean` or `{ offset, background }` or an array of sources
+  },
+  // shortcuts: [
+  // Your applications's Shortcuts (see: https://developer.mozilla.org/docs/Web/Manifest/shortcuts)
+  // Array of shortcut objects:
+  // {
+  //   name: 'View your Inbox', // The name of the shortcut. `string`
+  //   short_name: 'inbox', // optionally, falls back to name. `string`
+  //   description: 'View your inbox messages', // optionally, not used in any implemention yet. `string`
+  //   url: '/inbox', // The URL this shortcut should lead to. `string`
+  //   icon: 'test/inbox_shortcut.png', // source image(s) for that shortcut. `string`, `buffer` or array of `string`
+  // },
+  // more shortcuts objects
+  // ],
 };
 
 // https://vitejs.dev/config/
@@ -60,6 +106,7 @@ export default defineConfig(({ command: _, mode }) => {
       }),
       eslintPlugin({ eslintOptions: { cache: false } }),
       imageminPlugin(imageminConfig),
+      faviconsInjectPlugin('./src/assets/favicon.png', faviconsConfig),
     ],
   };
 });
