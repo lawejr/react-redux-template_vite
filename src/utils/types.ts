087f7739232e6777ab92193dcbc0c628b2e32e84
Aleksandr Kalinin <dev.lawejr@gmail.com>
2024-05-08 11:42:30 +0500

    type utils were added

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,4 +2,11 @@ function isObject(obj: unknown): obj is Record<string, any> {
   return !!obj && typeof obj === 'object' && !Array.isArray(obj);
 }
 
-export { isObject };
+function isNotNullOrUndefined<T>(obj: T): obj is Required<T> {
+  return obj !== undefined && obj !== null;
+}
+
+type ArrayElement<ArrayType extends readonly unknown[] | undefined> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export { isObject, isNotNullOrUndefined, type ArrayElement };
